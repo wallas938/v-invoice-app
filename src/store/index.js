29718056bfea95d075cf1279957266e2180c921c
Wallas9_3 <drame.sissako001@gmail.com>
2022-04-10 14:51:03 +0200

    invoice list not empty test is added

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,59 @@ import actions from './actions';
 export default createStore({
     state() {
         return {
-            invoices: []
+            invoices: [
+                {
+                    fromStreet: '19 Union Terrace',
+                    fromCity: 'London',
+                    fromPostCode: 'E1 3EZ',
+                    fromCountry: 'United Kingdom',
+                    clientName: 'Alex Grim',
+                    email: 'alexgrim@mail.com',
+                    toStreet: '84 Church Way',
+                    toCity: 'Bradford',
+                    toPostCode: 'BD1 9PB',
+                    toCountry: 'United Kingdom',
+                    invoiceDate: '21 Aug 2021',
+                    term: '20 Sep 2021',
+                    desc: 'Graphic Design',
+                    items: [],
+                    totalAmount: '£ 556.00',
+                },
+                {
+                    fromStreet: '19 Union Terrace',
+                    fromCity: 'London',
+                    fromPostCode: 'E1 3EZ',
+                    fromCountry: 'United Kingdom',
+                    clientName: 'Alex Grim',
+                    email: 'alexgrim@mail.com',
+                    toStreet: '84 Church Way',
+                    toCity: 'Bradford',
+                    toPostCode: 'BD1 9PB',
+                    toCountry: 'United Kingdom',
+                    invoiceDate: '21 Aug 2021',
+                    term: '20 Sep 2021',
+                    desc: 'Graphic Design',
+                    items: [],
+                    totalAmount: '£ 556.00',
+                },
+                {
+                    fromStreet: '19 Union Terrace',
+                    fromCity: 'London',
+                    fromPostCode: 'E1 3EZ',
+                    fromCountry: 'United Kingdom',
+                    clientName: 'Alex Grim',
+                    email: 'alexgrim@mail.com',
+                    toStreet: '84 Church Way',
+                    toCity: 'Bradford',
+                    toPostCode: 'BD1 9PB',
+                    toCountry: 'United Kingdom',
+                    invoiceDate: '21 Aug 2021',
+                    term: '20 Sep 2021',
+                    desc: 'Graphic Design',
+                    items: [],
+                    totalAmount: '£ 556.00',
+                }
+            ]
         }
     },
     mutations: mutations,
