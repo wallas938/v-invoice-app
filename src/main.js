4245d93d0958562117b3bf12dfd2c9a91d187d84
Wallas9_3 <drame.sissako001@gmail.com>
2022-05-13 01:01:52 +0200

    animation added for adding or removal a item field

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import './sass/_reset.scss';
 import './sass/_global.scss';
 import './sass/_mixins.scss';
 import './sass/_helpers.scss';
-
+import 'animate.css';
 /* 
     SINGLE-INSTANCE COMPONENTS 
 */
